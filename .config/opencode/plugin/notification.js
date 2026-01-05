9116186d9e716fa04ce1f0d9d5582345405aae40
Adam Spiers <dotfiles@adamspiers.org>
2026-01-05 12:33:46 +0000

    fix opencode notification plugin
    bun's $`...` seems horribly broken and can't handle multiple redirections.

--- .config/opencode/plugin/notification.js
+++ .config/opencode/plugin/notification.js
@@ -1,10 +1,28 @@
 export const NotificationPlugin = async ({ $ }) => {
+  // console.log("NotificationPlugin loaded!");
+  const fs = await import('fs');
+  const logPath = '/tmp/opencode-notification-plugin.log';
+
+  const logWithDate = (msg) => {
+    fs.appendFileSync(logPath, new Date().toISOString() + ' ' + msg + '\n');
+  };
+
   return {
     event: async ({ event }) => {
+      logWithDate(JSON.stringify(event));
       if (event.type === "session.idle") {
-        await $`mplayer -volume 50 /usr/share/sounds/Oxygen-Sys-App-Positive.ogg </dev/null >/dev/null`;
-        await $`notify-send 'opencode finished' >/dev/null`;
+          logWithDate(`Started handling ${event.type} event`);
+          $.throws(true);
+          try {
+              const result = await $`mplayer -volume 50 /usr/share/sounds/Oxygen-Sys-App-Positive.ogg &>> /tmp/wtf.log`;
+              // logWithDate('result: ' + JSON.stringify(result));
+              const result2 = await $`notify-send 'opencode finished' &>> /tmp/opencode-notify-send.log`;
+              // logWithDate(JSON.stringify(result2));
+          } catch (err) {
+              logWithDate(`ERROR: ${err}`);
+          }
       }
+      logWithDate(`Finished handling ${event.type} event`);
     },
   };
 };
